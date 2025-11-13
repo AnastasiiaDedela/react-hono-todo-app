64844feaf3eca44d899768f46390bf52ec1e1cbb
Anastasiia <nastyadedela@gmail.com>
2025-11-13 14:20:33 +0100

    feat: add loadind and error handling

--- client/src/routes/todos.tsx
+++ client/src/routes/todos.tsx
@@ -1,6 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { hc } from 'hono/client'
 import { useQuery } from '@tanstack/react-query'
+import { CircleX } from 'lucide-react'
 import type { AppType } from '../../../server/index'
 
 const client = hc<AppType>('/')
@@ -22,10 +23,31 @@ function RouteComponent() {
   })
 
   return (
-    <div>
-      {data?.map((todo) => {
-        return <div>{todo.title}</div>
-      })}
+    <div className="flex flex-col items-center p-10 gap-10">
+      {isError && (
+        <div role="alert" className="alert alert-error">
+          <CircleX />
+          <span>Error: {error.message}</span>
+        </div>
+      )}
+      <div className="space-y-3">
+        {isLoading && (
+          <>
+            {[1, 2, 3, 4, 5].map(() => (
+              <div className="flex flex-row items-center gap-3">
+                <div className="skeleton h-7 w-7 rounded-full"></div>
+                <div className="skeleton h-6 w-32"></div>
+              </div>
+            ))}
+          </>
+        )}
+        {data?.map((todo) => (
+          <div className="flex flex-row items-center gap-3">
+            <input type="checkbox" className="checkbox checkbox-primary" />
+            <div>{todo.title}</div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
